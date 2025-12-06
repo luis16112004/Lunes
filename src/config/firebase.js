019d3684a06a8c4bfd2d8f0110c08572aa9e1cdb
luisc <luisc@LUIS16>
2025-12-05 22:10:07 -0600

    Initial commit: API con Firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,23 @@
 const admin = require('firebase-admin');
 
-// Para desarrollo local
-const serviceAccount = require('C:\Users\luisc\Downloads');
+let credential;
+
+if (process.env.NODE_ENV === 'production') {
+  credential = admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  });
+} else {
+  const serviceAccount = require('../../firebase-credentials.json');
+  credential = admin.credential.cert(serviceAccount);
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: credential,
+  databaseURL: 'https://backendjaminfirebase-default-rtdb.firebaseio.com'
 });
 
-const db = admin.firestore();
+const db = admin.database(); // Cambia a database() en lugar de firestore()
 
 module.exports = { admin, db };
